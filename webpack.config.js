470f43d26859844b268289b57909220b348c8e66
Vladislav Biryukov <vobiryukov12@gmail.com>
2023-07-05 18:20:53 +0300

    1 / Change the webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,13 @@ module.exports = {
         }, {
           loader: 'svgo-loader'
         }]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: './fonts/[name][ext]'
+        }
       }
     ]
   },
