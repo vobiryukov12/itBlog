0791fb23dca2dc640e3b71fbe0fa849e1e2488ff
Vladislav Biryukov <vobiryukov12@gmail.com>
2023-10-16 15:41:53 +0300

    fix: fix page element access error

--- assets/js/modules/show-more.js
+++ assets/js/modules/show-more.js
@@ -6,7 +6,7 @@ const ELEMENTS_SELECTOR = {
 const buttonArticle = document.querySelector(ELEMENTS_SELECTOR.buttonArticle);
 const buttonNews = document.querySelector(ELEMENTS_SELECTOR.buttonNews);
 
-buttonArticle.addEventListener('click', () => {
+buttonArticle && buttonArticle.addEventListener('click', () => {
   const hidden = document.querySelectorAll('.cards__hidden-article');
   for (let i = 0; i < 3 && i < hidden.length; i++) {
     hidden[i].classList.remove('cards__hidden-article');
@@ -17,7 +17,7 @@ buttonArticle.addEventListener('click', () => {
   }
 });
 
-buttonNews.addEventListener('click', () => {
+buttonNews && buttonNews.addEventListener('click', () => {
   const hidden = document.querySelectorAll('.cards__hidden-news');
   for (let i = 0; i < 1 && i < hidden.length; i++) {
     hidden[i].classList.remove('cards__hidden-news');
